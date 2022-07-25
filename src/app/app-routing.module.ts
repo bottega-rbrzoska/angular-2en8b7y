21d53f6aa799138b9796011a14a412e3bfc58528
Rafal Brzoska <rbrzoska@future-processing.com>
2022-07-25 12:55:17 +0200

    first components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyTestComponent } from './my-test/my-test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'test', component: MyTestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/my-test/my-test.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dtiq-my-test',
+  templateUrl: './my-test.component.html',
+  styleUrls: ['./my-test.component.scss']
+})
+export class MyTestComponent implements OnInit {
+
+  testData = 'myTestString';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
